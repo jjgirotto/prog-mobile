d8cb66ac1061e2bca63064ff2e2b35f18b4983a5
jjgirotto <ads.jjgirotto@gmail.com>
2025-11-25 17:07:47 +0000

    app lista iniciado

---
+++ Lista/app/src/main/java/com/example/lista/VisualAnimalActivity.java
@@ -0,0 +1,18 @@
+package com.example.lista;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class VisualAnimalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visual_animal);
+    }
+}
---
+++ Lista/app/src/main/java/com/example/lista/classes/Animal.java
@@ -0,0 +1,37 @@
+package com.example.lista.classes;
+
+public class Animal {
+    private int photo;
+    private String name;
+    private String habitat;
+
+    public Animal(int photo, String name, String habitat) {
+        setPhoto(photo);
+        setName(name);
+        setHabitat(habitat);
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+}
